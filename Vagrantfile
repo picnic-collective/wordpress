c547625b1e8bf9467ee10becc373c5e2658c9a90
Mark Wales <mark@smallhadroncollider.com>
2015-04-15 20:37:14 +0100

    added Vagrantfile

---
+++ Vagrantfile
@@ -0,0 +1,26 @@
+# Set the site name
+site_name = "xxx"
+
+Vagrant.configure(2) do |config|
+  config.vm.box = "smallhadroncollider/ubuntu-14.04-nginx-php55-mysql"
+  config.vm.box_version = "0.2"
+
+  # Setup the dev domain name
+  config.vm.network "private_network", ip: "172.31.254.245"
+  config.vm.hostname = "#{site_name}.dev"
+  config.hostsupdater.aliases = ["www.#{site_name}.dev"]
+
+  # Setup the server root
+  config.vm.synced_folder "./", "/var/www"
+
+  # Add provisioning
+  config.vm.provision "shell", path: "provision.sh"
+
+  # Configure VirtualBox
+  config.vm.provider :virtualbox do |virtualbox|
+    virtualbox.name = site_name
+    virtualbox.memory = 512
+    virtualbox.cpus = 1
+    virtualbox.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+  end
+end
---
+++ provision.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+# Variables
+db="xxx"
+password="xxx"
+
+# Create databases
+echo "
+    CREATE DATABASE ${db} CHARACTER SET utf8 COLLATE utf8_general_ci;
+    CREATE USER '${db}'@'localhost' IDENTIFIED BY '${password}';
+    GRANT ALL PRIVILEGES ON ${db}.* TO '${db}'@'localhost';
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root';
+    FLUSH PRIVILEGES;
+" | mysql -u root
+
+# Add rewrite rules to Nginx
+echo "echo 'server {
+    listen 80 default_server;
+
+    root /var/www/public;
+    index index.php;
+
+    server_name localhost;
+
+    location / {
+         try_files \$uri \$uri/ /index.php\$is_args\$args;
+    }
+
+    location ~ \.php\$ {
+        fastcgi_split_path_info ^(.+\.php)(/.+)\$;
+        fastcgi_pass unix:/var/run/php5-fpm.sock;
+        fastcgi_index index.php;
+        include fastcgi_params;
+    }
+}' > /etc/nginx/sites-available/default" | sudo sh
+sudo nginx -s reload
+
+# Remove bind address - allows remote MySQL
+sudo sed -r -i "s/bind\-address/#bind\-address/" /etc/mysql/my.cnf
+sudo service mysql restart
