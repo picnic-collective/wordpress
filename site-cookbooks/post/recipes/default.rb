fe58a65fa12a4da176307d8c820e612b184a9e4d
Mark Wales <mark@smallhadroncollider.com>
2015-04-18 20:31:18 +0100

    fixed duplicated task in post chef recipe

--- site-cookbooks/post/recipes/default.rb
+++ site-cookbooks/post/recipes/default.rb
@@ -2,7 +2,7 @@
 theme_dir = "#{node["wordpress"]["dir"]}/wp-content/themes/#{node["wordpress"]["db"]["name"]}";
 
 execute "create_theme_directory" do
-    command "mkdir /vagrant/theme && ln -s ../../../theme #{theme_dir}"
+    command "mkdir /vagrant/theme"
     not_if { ::File.exists?("/vagrant/theme")}
 end
 
